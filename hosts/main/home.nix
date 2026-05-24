91ce7dc8603abd87cc7295b816efd807ecf6ef8b
Liam McAuliffe <lmmcauliffe1223@gmail.com>
2026-05-24 12:34:23 -0400

    home.nix to lua for hyprland

--- hosts/main/home.nix
+++ hosts/main/home.nix
@@ -2,23 +2,32 @@
 {
   home.stateVersion = "26.05";
 
-  wayland.windowManager.hyprland = {
-    enable = true;
-    extraConfig = ''
-      monitor = ",preferred,auto,1"
+  wayland.windowManager.hyprland.enable = true;
 
+  home.file.".config/hypr/hyprland.lua".text = ''
+    local terminal = "ghostty"
+    local mainMod  = "SUPER"
+
+    hl.monitor({
+      output   = "",
+      mode     = "preferred",
+      position = "auto",
+      scale    = "auto",
+    })
+
+    hl.config({
       general = {
-        gaps_in = 5;
-        gaps_out = 10;
-        border_size = 2;
-      }
-
+        gaps_in     = 5,
+        gaps_out    = 10,
+        border_size = 2,
+      },
       decoration = {
-        rounding = 10;
-      };
+        rounding = 10,
+      },
+    })
 
-      bind = SUPER, Return, exec, ghostty
-      bind = SUPER, Q, killactive
-    '';
-  };
+    hl.bind(mainMod .. " + Return", hl.dsp.exec_cmd(terminal))
+    hl.bind(mainMod .. " + C",      hl.dsp.window.close())
+    hl.bind(mainMod .. " + M",      hl.dsp.exit())
+  '';
 }
