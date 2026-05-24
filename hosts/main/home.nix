26310b6bbf61cfb6666d26bc12d6ead9826b8efa
Liam McAuliffe <lmmcauliffe1223@gmail.com>
2026-05-24 10:34:37 -0400

    fix home.nix

--- hosts/main/home.nix
+++ hosts/main/home.nix
@@ -5,18 +5,20 @@
   wayland.windowManager.hyprland = {
     enable = true;
     extraConfig = ''
-      monitor = ",preferred,auto,1";
+      monitor = ",preferred,auto,1"
+
       general = {
         gaps_in = 5;
         gaps_out = 10;
         border_size = 2;
-      };
+      }
+
       decoration = {
         rounding = 10;
       };
-      bind = [
-        "SUPER, Return, exec, ghostty"
-        "SUPER, Q, killactive"
-      ];
+
+      bind = SUPER, Return, exec, ghostty
+      bind = SUPER, Q, killactive
     '';
   };
+}
