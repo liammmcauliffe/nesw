779428596a07768f7d753ae300e22a1a79749083
Liam McAuliffe <lmmcauliffe1223@gmail.com>
2026-05-28 12:19:55 -0400

    changes to hyprland keybinds

---
+++ modules/hyprland/config/keybinds.lua
@@ -0,0 +1,8 @@
+local vars = require("variables")
+
+hl.bind("SUPER + mouse_down", hl.dsp.focus({ workspace = "-1" }))
+hl.bind("SUPER + mouse_up", hl.dsp.focus({ workspace = "+1" }))
+hl.bind(vars.kbPrevWs, hl.dsp.focus({ workspace = "-1" }), { repeating = true })
+hl.bind(vars.kbNextWs, hl.dsp.focus({ workspace = "+1" }), { repeating = true })
+hl.bind("SUPER + Page_Up", hl.dsp.focus({ workspace = "-1" }), { repeating = true })
+
