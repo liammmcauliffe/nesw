131c163eb69a86145c54abf21dfcb3a0553c2a22
Liam McAuliffe <lmmcauliffe1223@gmail.com>
2026-06-13 22:28:27 -0400

    fix guesters

--- modules/hyprland/config/gestures.lua
+++ modules/hyprland/config/gestures.lua
@@ -3,6 +3,8 @@ local vars = require("variables")
 -- gesture config
 hl.config({
     gestures = {
+        workspace_swipe = true,
+        workspace_swipe_fingers = vars.workspaceSwipeFingers,
         workspace_swipe_distance = 700,
         workspace_swipe_cancel_ratio = 0.15,
         workspace_swipe_min_speed_to_force = 5,
@@ -12,19 +14,7 @@ hl.config({
     },
 })
 
-local function ws_step(delta)
-    return function()
-        if delta > 0 then
-            hl.dispatch(hl.dsp.focus({ workspace = "+1" }))
-        else
-            hl.dispatch(hl.dsp.focus({ workspace = "-1" }))
-        end
-    end
-end
-
 -- gestures
-hl.gesture({ fingers = vars.workspaceSwipeFingers, direction = "left", action = ws_step(1) })
-hl.gesture({ fingers = vars.workspaceSwipeFingers, direction = "right", action = ws_step(-1) })
 hl.gesture({ fingers = vars.gestureFingers, direction = "up", action = "special", arg = "special" })
 hl.gesture({
     fingers = vars.gestureFingersMore,
