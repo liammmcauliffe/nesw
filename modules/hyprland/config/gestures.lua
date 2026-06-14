50ddac9f5e044598db9f3c6c8eac19d3decfa716
Liam McAuliffe <lmmcauliffe1223@gmail.com>
2026-06-13 22:52:37 -0400

    fix erros

--- modules/hyprland/config/gestures.lua
+++ modules/hyprland/config/gestures.lua
@@ -3,8 +3,6 @@ local vars = require("variables")
 -- gesture config
 hl.config({
     gestures = {
-        workspace_swipe = true,
-        workspace_swipe_fingers = vars.workspaceSwipeFingers,
         workspace_swipe_distance = 700,
         workspace_swipe_cancel_ratio = 0.15,
         workspace_swipe_min_speed_to_force = 5,
@@ -14,7 +12,8 @@ hl.config({
     },
 })
 
--- gestures
+-- gestures (Hyprland 0.51+: workspace swipe is a gesture action, not a toggle)
+hl.gesture({ fingers = vars.workspaceSwipeFingers, direction = "horizontal", action = "workspace" })
 hl.gesture({ fingers = vars.gestureFingers, direction = "up", action = "special", arg = "special" })
 hl.gesture({
     fingers = vars.gestureFingersMore,
